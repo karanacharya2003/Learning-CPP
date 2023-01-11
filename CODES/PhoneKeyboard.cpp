2f0e724b44ada50e6283ac76387beaa112d4c2f1
karanacharya2003 <72179869+karanacharya2003@users.noreply.github.com>
2023-01-11 20:46:15 +0530

    Create PhoneKeyboard.cpp

---
+++ CODES/PhoneKeyboard.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+    }
+};
+
+ void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        //base case
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+        
+        int number = digit[index] - '0';
+        string value = mapping[number];
+        
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+          
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+   }
+  vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+      
+        if(digits.length()==0){
+            return ans;
+          }
+        string output;
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+ 
+    return ans;
+     
+   
+    }
+
+int main() {
+
+  string digit="34";
+  vector<string> arr=letterCombinations(digit);
+    std::cout << "Hello World!\n";
+ for(int i=0; i<arr.size();i++){
+    cout<<arr.at(i)<<endl;
+  }
+  }
